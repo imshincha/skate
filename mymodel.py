1c25b8c608bfbca95a7b85cbbe5e4d8735b8b4d2
imshincha <64887013+imshincha@users.noreply.github.com>
2021-02-15 18:13:58 +0900

    Update mymodel.py

--- mymodel.py
+++ mymodel.py
@@ -2,5 +2,4 @@ import timm
 from pprint import pprint
 model_names = timm.list_models(pretrained = True)
 model = timm.create_model('efficientnet_b3a', pretrained = True)
-pprint(model_names)
 model.eval()
